6163d974170041a7226719c8f8cc204e404e7194
VladSkopenko <skopirka2k17@gmail.com>
2024-08-17 01:26:20 +0300

    first version app

--- data9/data9/urls.py
+++ data9/data9/urls.py
@@ -18,8 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('image_analysis.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
