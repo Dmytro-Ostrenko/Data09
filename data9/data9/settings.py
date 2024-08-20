8b81adf8c9295d764b60559089b4dc16fa95e56b
VladSkopenko <skopirka2k17@gmail.com>
2024-08-20 18:51:25 +0300

    refactor

--- data9/data9/settings.py
+++ data9/data9/settings.py
@@ -134,6 +134,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MODELS_ROOT = os.path.join(BASE_DIR, 'models')
 
 # Cloudinary settings
 cloudinary.config(
